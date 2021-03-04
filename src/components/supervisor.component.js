42c49e553bfb8bc56e1b0e16f2d0b18a31a208ba
shalin_shah_21 <shalin.9230@gmail.com>
2021-03-04 20:04:25 +0400

    Some Changes

--- src/components/supervisor.component.js
+++ src/components/supervisor.component.js
@@ -4,46 +4,17 @@ import UserService from '../services/user.service.js';
 
 class SupervisorDashboard extends Component {
   
-  state = {
-    arr: ["rama", "shyama"],
-    responseData: '',
-  }
 
-  componentDidMount = async () => {
+  componentDidMount() {
     // call component specific api here
-        const { user: currentUser={} } = this.props;
-
-    try {
-
-
-	const response = await UserService.getModeratorBoard(currentUser.email);
-
-	console.log(' response',response)
-
-	const {status,data} = response;
-	if(status === 200){
-		this.setState({
-			
-			responseData: data
-		});
-	}
-
-
-}catch(e){
-	console.log(' errror ',e)
-}
   }
+
+
   
   render() {  
-  	const {arr} = this.state;
     return (
-    	<>
     	<h3> Welcome to Supervisor dashboard</h3>
-    	<h4>{this.state.responseData}</h4>
-    	<ul>
-          { arr.map((role, index) =><li key={index}>{role}</li>)}
-        </ul>
-        </>
+    
     );
   }
 }
